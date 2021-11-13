e2ef5c9293473083593c38b473a7d6627cc6779f
Frank William Daniels <frankwd@stud.ntnu.no>
2021-11-13 12:20:17 +0100

    Implemented cypress test for league filter and added ID to client search fields

--- cypress/integration/Football-browser-tests/filter_spec.js
+++ cypress/integration/Football-browser-tests/filter_spec.js
@@ -6,37 +6,27 @@ describe('Filter', () => {
         cy.visit('http://localhost:3000/prosjekt4')
     })
 
+    //1. filter
+    //2. sort by league
+    //3. sort by league again
     it('Filters by Leauge', () => {
-
-        
-    })
-
-
-
-
-/*     it('Sorts by Leauge', () => {
-      //Sort for league
-      cy.get(`[id="selectSort"]`).select("league")
-      cy.get(`[id="sortButton"]`).click()
-
+      cy.get(`[id="filterLeague"]`).type("SP1")
+      cy.get(`[id="filterAndSearchButton"]`).click()
       cy.wait("@getGames")
       cy.wait("@getGames").then((gameInterception1) => {
         const leagueBefore = gameInterception1.response.body.data.GetGamesFilterList.games[0].league;
-
-        //Sort again
+        cy.get(`[id="selectSort"]`).select("league")
         cy.get(`[id="sortButton"]`).click()
-        cy.wait("@getGames").then((gameInterception2) => {
-          const leagueAfter = gameInterception2.response.body.data.GetGamesFilterList.games[0].league;
+        cy.wait("@getGames").then((gameInterception1) => {
+          const leagueAfter = gameInterception1.response.body.data.GetGamesFilterList.games[0].league;
           //localeCompare() does lexicographical comparison
           //Return -1, 0 or 1.
           const comparison = leagueAfter.localeCompare(leagueBefore);
           //Returns 1 if league after (spain) is lexicographic greater than league before sort (england)
-          expect(comparison === 1).to.be.true
+          expect(comparison === 0).to.be.true
         })
       })
-    }) */
-
-
+    })
 
 
 
