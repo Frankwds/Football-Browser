4594685afa736cc1da99843e128fd02e109a8b81
Frank William Daniels <frankwd@stud.ntnu.no>
2021-11-17 14:15:15 +0100

    Added tests for game modal

--- cypress/integration/Football-browser-tests/modal_spec.js
+++ cypress/integration/Football-browser-tests/modal_spec.js
@@ -17,7 +17,7 @@ describe('Game modal, rating and commenting', () => {
         
     // })
 
-    it('Game modal can open', () => {
+    it.skip('Game modal can open', () => {
       // Wait for game and user data to load
       cy.wait('@getGames')
       cy.wait('@userData')
@@ -33,12 +33,47 @@ describe('Game modal, rating and commenting', () => {
 
 
 
-    // it('Can Close by X', () => {
-        
-    // })
-    // it('Can Close by clicking outside', () => {
-        
-    // })
+    it('Can Close by X', () => {
+              // Wait for game and user data to load
+      cy.wait('@getGames')
+      cy.wait('@userData')
+      cy.wait('@userData')
+      cy.wait('@userData')
+      
+      // Click first game
+      cy.get('.Result').children().eq(1).click()
+
+      // Confirm modal text is there
+      cy.contains('The id of the game is:')
+
+      //Close modal
+      cy.get(`[id="CloseModal"]`).click()
+      // Confirm modal text is not there
+      cy.wait(200)
+      cy.contains('The id of the game is:').should('not.exist')
+    })
+
+
+
+    it('Can Close by clicking outside', () => {
+                      // Wait for game and user data to load
+      cy.wait('@getGames')
+      cy.wait('@userData')
+      cy.wait('@userData')
+      cy.wait('@userData')
+      
+      // Click first game
+      cy.get('.Result').children().eq(1).click()
+
+      // Confirm modal text is there
+      cy.contains('The id of the game is:')
+
+      //Close modal
+      cy.get('body').click(10,10);
+      // Confirm modal text is not there
+      cy.wait(200)
+      cy.contains('The id of the game is:').should('not.exist')
+    })
 })
 
 
