5a1c39e5823554eefd89667958755eb00609d645
Frank William Daniels <frankwd@stud.ntnu.no>
2021-11-17 11:52:28 +0100

    Fixed Providers around PageChanger so they run as expected.

--- src/components/sortingComponents/__tests__/PageChanger.test.tsx
+++ src/components/sortingComponents/__tests__/PageChanger.test.tsx
@@ -1,15 +1,27 @@
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
 import renderer from "react-test-renderer";
+import { createStore } from "redux";
 import PageChanger from "../PageChanger";
+import allReducers from "../../../redux";
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 
+// Component input parameters
 const col = "rgb(192,192,192)";
 const acc = "rgb(0,128,128)";
+
+// Test <div> container
 let container: any;
+
+// Set up redux store
+const store = createStore(allReducers);
+
 
 beforeEach(() => {
   container = document.createElement("div");
   document.body.appendChild(container);
+  
 });
 
 afterEach(() => {
@@ -17,19 +29,30 @@ afterEach(() => {
   container = null;
 });
 
+{/* <ApolloProvider client={client}>
+
+</ApolloProvider> */}
+
+
+
 describe("Testing PageChanger", () => {
   it("should render without crashing", () => {
     act(() => {
       const div = document.createElement("div");
       ReactDOM.render(
-        <PageChanger
-          page={0}
-          lastpage={10}
-          pageSize={5}
-          func={() => {}}
-          color={col}
-          accent={acc}
-        />,
+        <Provider store={store}>
+          <ChakraProvider>
+            <PageChanger
+              page={0}
+              lastpage={10}
+              pageSize={5}
+              func={() => {}}
+              color={col}
+              accent={acc}
+            />
+          </ChakraProvider>
+        </Provider>
+        ,
         div
       );
     });
@@ -38,14 +61,18 @@ describe("Testing PageChanger", () => {
   it("snapshot should be same as previous", () => {
     const tree = renderer
       .create(
-        <PageChanger
-          page={0}
-          lastpage={10}
-          pageSize={5}
-          func={() => {}}
-          color={col}
-          accent={acc}
-        />
+        <Provider store={store}>
+          <ChakraProvider>
+            <PageChanger
+              page={0}
+              lastpage={10}
+              pageSize={5}
+              func={() => {}}
+              color={col}
+              accent={acc}
+            />
+          </ChakraProvider>
+        </Provider>
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
