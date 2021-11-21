8002b2e388a196a572437c5f2bff1a802bd015d2
Gerhard <gerhargu@stud.ntnu.no>
2021-11-21 14:57:13 +0100

    made Headertest test for image

--- src/components/containerComponents/__tests__/Header.test.tsx
+++ src/components/containerComponents/__tests__/Header.test.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import Header from "../Header";
 import renderer from "react-test-renderer";
 import { act } from "react-dom/test-utils";
-
+import logoImage from "./../../../resources/football.jpg";
 
 let container: any;
 
@@ -19,16 +19,31 @@ afterEach(() => {
 describe("Testing Header", () => {
   it("should render without crashing", () => {
     act(() => {
-      const div = document.createElement("div");
-      ReactDOM.render(<Header />, div);
-    })
+      ReactDOM.render(<Header />, container);
+    });
   });
 
   it("snapshot should be same as previous", () => {
-    const tree = renderer
-      .create(<Header />)
-      .toJSON();
+    const tree = renderer.create(<Header />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-});
 
+  it("testing if footer renders propperly", () => {
+    act(() => {
+      ReactDOM.render(<Header />, container);
+    });
+    expect(container.querySelector("h2").textContent).toBe(
+      "WebDev Project3 - Football browser"
+    );
+  });
+
+  it("renders the correct image", () => {
+    act(() => {
+      ReactDOM.render(<Header />, container);
+    });
+
+    expect(container.querySelector("img").getAttribute("src")).toEqual(
+      logoImage
+    );
+  });
+});
